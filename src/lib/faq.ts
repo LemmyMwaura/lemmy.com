f693580a8f3e9c3f499dcc7531ae4f2633fb53ab
lemmy mwaura <lemminton@gmail.com>
2023-01-22 13:17:53 +0300

    feat: update faq

--- src/lib/faq.ts
+++ src/lib/faq.ts
@@ -1,11 +1,27 @@
 export const FAQs = [
   {
+    question: "When did you start coding?",
+    answer: "Officialy since Sep 30th 2021 but i've been writing coding since I was in HighSchool.",
+  },
+  {
+    question: "Why did you stop?",
+    answer: "I've been passionate about Tech my entire life and I loved this field too much to ever do it without that sense of purpose and it just wasn't there at the time so i took a step back.",
+  },
+  {
+    question: "Why did come back?",
+    answer: "Honestly, I did miss writing code and all the signs led this way. I also do find a sense of purpose in this. lol",
+  },
+  {
+    question: "What do you hope to achieve in your career?",
+    answer: "Easy, Create Amazing products/solutions, Work with Amazing people and do this as often as i can.",
+  },
+  {
     question: "What programming languages do you know?",
-    answer: "I'm most profficient in Typescript and Python. I also write some Go.",
+    answer: "I'm most profficient in javaScript, Typescript and Python. I also write some Go.",
   },
   {
     question: "What's your view on Typesafety?",
-    answer: "I think good Typesafety is essential to writing amazing javaScript code and it should never be sacrifised.",
+    answer: "You should never sacrifice typesafety. Ts all the way.",
   },
   {
     question: "What are you working on nowadays?",
@@ -13,7 +29,7 @@ export const FAQs = [
   },
   {
     question: "What is your favourite web framework?",
-    answer: "NextJs has got to be my favourite framework. The ability to decide the rendering pattern that's best for your app and the amazing list of libraries build by the community around the framework make it really easy to build full-stack typesafe and fast webapps, plus the new changes introduced in Next13 with server components will make the rendering patterns even more easy and intuitive. I also like Angular, Sveltekit and Astro.",
+    answer: "NextJs has got to be my favourite framework. The ability to choose the rendering pattern that's best for your app and the amazing list of libraries built by the community around the framework make it really easy to build full-stack typesafe and fast webapps, plus the new updates introduced in Next13 with server components will make the rendering patterns even more easy to implement and very intuitive. I also like Angular, Sveltekit and Astro.",
   },
   {
     question: "Thoughts on Angular?",
